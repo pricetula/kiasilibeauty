7e09c7e6c37aa26e49c2818f433b54036d07496b
price tula <pricetulak@gmail.com>
2021-04-12 21:11:27 +0300

    changes

---
+++ out/_next/static/bOcKz58Rv5tUErSlwxc5D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-a81396d9c8454c229a65.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d1aa0654592d0ac6608c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
